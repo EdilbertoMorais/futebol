57b0a93ef3a4aaf3ca9af756679e20488cd6f60c
Edilberto Morais <edilbertocmorais@gmail.com>
2022-09-02 17:42:42 -0300

    inicio do projeto futebol_api

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :extwitter, :oauth,
-  consumer_key: "",
-  consumer_secret: "",
-  access_token: "",
-  access_token_secret: ""
+  consumer_key: System.get_env("CONSUMER_KEY"),
+  consumer_secret: System.get_env("CONSUMER_SECRET"),
+  access_token: System.get_env("ACCESS_TOKEN"),
+  access_token_secret: System.get_env("ACCESS_TOKEN_SECRET")
--- lib/futebol.ex
+++ lib/futebol.ex
@@ -12,7 +12,32 @@ defmodule Futebol do
       :world
 
   """
-  def hello do
-    :world
+
+  @base_url "https://api.api-futebol.com.br/v1/campeonatos/14/rodadas/28"
+  @headers [
+    Accept: "application/json",
+    Authorization: "Bearer test_a51707e2019b9da11a931c4a8341f3"
+  ]
+
+  def futebol_info do
+    {:ok, response} = HTTPoison.get(@base_url, @headers)
+    {:ok, body} = Jason.decode(response.body)
+
+    %{"partidas" => partidas} = body
+
+    lista_placar = Enum.map(partidas, fn partida -> partida["placar"] end)
+
+    publicacao = Enum.join(lista_placar, "\n")
+
+    ExTwitter.update(publicacao)
   end
 end
+
+# headers = ["Accept": "application/json", "Authorization": "Bearer test_a51707e2019b9da11a931c4a8341f3"]
+# url = "https://api.api-futebol.com.br/v1/campeonatos/14/rodadas/28"
+# HTTPoison.get(url, headers)
+# {:ok, response} =  HTTPoison.get(url, headers)
+# response.body
+# Jason.decode(response.body)
+# {:ok, body} = Jason.decode(response.body)
+#
